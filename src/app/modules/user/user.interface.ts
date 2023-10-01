8d4f7ebeac039695860dada5bc27843893aa51dd
Shahriar Ahmed <ourshahriar@gmail.com>
2023-10-01 22:09:51 +0600

    Added

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,20 @@
+export interface IUser {
+  uid: string;
+  role: "student" | "teacher" | "admin";
+  email: string;
+  phone: string;
+  password: string;
+  name: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+  };
+  dateOfBirth: string;
+  gender: "male" | "female" | "other";
+  contactNo: string;
+  emergencyContactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  bloodGroup: "A+" | "B+" | "AB+" | "O+" | "A-" | "B-" | "AB-" | "O-";
+  residency: string;
+}
